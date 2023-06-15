4750cdf023bebd2ffc2ff911c0bc3dd9fb069183
samarth <samarthtandon91@gmail.com>
2023-06-15 23:48:29 +0530

    added check for call duration ignore if duration less than 1min

--- src/conversation_bot/data_orchestrator/data_processing.py
+++ src/conversation_bot/data_orchestrator/data_processing.py
@@ -11,10 +11,11 @@ from signal_handler.constant import WHISPER_MODEL,DEVICE,DOCUMENT_OUT_PATH
 
 
 class DataOrchestrator:
-    def __init__(self, ):
+    def __init__(self, min_call_duration_in_seconds = 100):
         self.conversation_dict = dict()
         self.whisper_model_name = WHISPER_MODEL
         self.device = DEVICE
+        self.min_duration = min_call_duration_in_seconds
         self.audio_process = AudioProcess(model_name=self.whisper_model_name,
                                           device=self.device
                                           )
@@ -23,23 +24,30 @@ class DataOrchestrator:
     def process_records(self, records_list):
         self.conversation_dict['data'] = list()
         for audio_path in tqdm(records_list):
-            if "http://" in audio_path:
-                r = requests.get(audio_path)
-                if r.status_code == 200:
-                    self.process_audio(audio_path)
+            dir_path = os.path.dirname(audio_path)
+            filename = os.path.basename(audio_path)
+            json_file = os.path.join(dir_path,filename[:-3]+'json')
+            if not os.path.isfile(json_file):
+                if "http://" in audio_path:
+                    r = requests.get(audio_path)
+                    if r.status_code == 200:
+                        self.process_audio(audio_path)
+                    else:
+                        logging.error("Unable to reach for URL {}".format(audio_path))
+                        continue
                 else:
-                    logging.error("Unable to reach for URL {}".format(audio_path))
-                    continue
-            else:
-                self.process_audio(audio_path)
+                    self.process_audio(audio_path)
+                self.save_record(record=self.conversation_dict,out_path=json_file)
 
         self.save_record(record=self.conversation_dict, out_path=self.out_path)
         return self.out_path
 
     def process_audio(self, audio_path):
         text, duration, lang, conv_info = self.audio_process.speech_to_text(audio_path)
+        if duration < self.min_duration:
+            return
         text = re.sub("\s\s+", " ", text)
-        text = "conversation between Customer {} and sales {} in {} language:{}".format(
+        text = "conversation between Customer {} and relationship manager {} in {} language:{}".format(
             conv_info['customer_name'], conv_info['representative_name'], lang, text)
         self.conversation_dict['data'].append({
             'audio_url': audio_path,
