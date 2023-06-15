a9944c5875de410cfd244696ca0ac0c290d35598
samarth <samarthtandon91@gmail.com>
2023-06-15 23:38:55 +0530

    added STREAMLIT path

--- src/conversation_bot/main.py
+++ src/conversation_bot/main.py
@@ -10,7 +10,7 @@ os.environ['OPENAI_API_KEY'] = OPENAPI_KEY
 
 
 def data_creation():
-    audio_files = [osp.join(AUDIO_FILES_DIR, fname) for fname in os.listdir(AUDIO_FILES_DIR)]
+    audio_files = [osp.join(AUDIO_FILES_DIR, fname) for fname in os.listdir(AUDIO_FILES_DIR) if fname.endswith('.mp3')]
     data_orchestrator = DataOrchestrator()
     text_docments = data_orchestrator.process_records(audio_files)
     IngestionToVectorDb()
@@ -35,8 +35,7 @@ if __name__ == '__main__':
                         help='Read Audio data (url/file) and create embeddings')
     parser.add_argument('--create_vector', action='store_true', default=None, help='Ingest vector Embeddings')
     parser.add_argument('--app', action='store_true', default=None, help='run application')
-    parser.add_argument('--openapi_key', default='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',
-                        help='required API KEY')
+    parser.add_argument('--openapi_key', default=None,help='required API KEY')
 
     args = parser.parse_args()
     main(args)
