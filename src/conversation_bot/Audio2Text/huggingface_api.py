6a16b9c78d71ac06caefadf932538d301cf37737
samarth991 <samarthtandon91@gmail.com>
2023-06-28 15:27:12 +0530

    updated hugging

--- src/conversation_bot/Audio2Text/huggingface_api.py
+++ src/conversation_bot/Audio2Text/huggingface_api.py
@@ -1,26 +1,42 @@
 import os
+import time
+
 import torch as th
 from transformers import Wav2Vec2Processor, Wav2Vec2ForCTC
+import nltk
 import re
-from googletrans import Translator
-import torchaudio
-# import huggingface_hub
+import logging
+import librosa
+import huggingface_hub
+import soundfile as sf
+import io
+from urllib.request import urlopen
 
+nltk.download('punkt')
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 
-# os.environ['OPENAI_API_KEY'] = "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"
-os.environ['HUGGINGFACEHUB_API_TOKEN'] = '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'
-# huggingface_hub.login(token=os.getenv('HUGGINGFACEHUB_API_TOKEN'))
+# os.environ['OPENAI_API_KEY'] = "sk-<>"
+os.environ['HUGGINGFACEHUB_API_TOKEN'] = ''
+
+huggingface_hub.login(token=os.getenv('HUGGINGFACEHUB_API_TOKEN'))
 
 
 def load_audio(audio_url):
     """Load the audio file & convert to 16,000 sampling rate"""
-    speech, sr = torchaudio.load(audio_url)
-    if sr != 16000:
-        resampler = torchaudio.transforms.Resample(sr, 16000)
-        speech = resampler(speech)
-    duration = speech.shape[1] / 16000
-    return speech.squeeze(), duration
+    speech, fs = sf.read(io.BytesIO(urlopen(url).read()))
+    if len(speech.shape) > 1:
+        speech = speech[:, 0] + speech[:, 1]
+    if fs != 16000:
+        speech = librosa.resample(speech, orig_sr=fs, target_sr=16000)
+    duration = speech.shape[0] / 16000
+    sf.write("tmp.wav", speech, 16000)
+    time.sleep(1)
+    return speech, duration
+
+
+def correct_sentences(input_text):
+    words_array = nltk.word_tokenize(input_text)
+    return " ".join(words_array)
 
 
 def run_inference(speech_clip, model, processor, device='cpu'):
@@ -30,78 +46,79 @@ def run_inference(speech_clip, model, processor, device='cpu'):
     logits = model(input_features)["logits"]
     predicted_ids = th.argmax(logits, dim=-1)
     transcription = processor.batch_decode(predicted_ids)[0]
-    return transcription.lower()
+    return " ".join(correct_sentences(transcription))
 
 
 def get_transcription(audio_path, model, processor, device='cpu', n_seconds=30):
     speech, duration = load_audio(audio_path)
-    initial_index = 0
-    text = []
+    translated_text = []
+    predicted_text = []
+
     if duration > n_seconds:
-        for index in range(int(duration / n_seconds)):
-            num_samples = initial_index + n_seconds * int((speech.shape[0] / 16000))
-            speech_clip = speech[initial_index:num_samples]
-
-            predicted_text = run_inference(speech_clip, model, processor, device=device)
-            arabic_to_en = translate_text(predicted_text)
-            print(arabic_to_en)
-            text.append(re.sub(r'[^\w]', ' ', arabic_to_en))
-            initial_index = num_samples
+        stream = librosa.stream(
+            "tmp.wav",
+            block_length=n_seconds,
+            frame_length=16000,
+            hop_length=16000
+        )
+        for speech_clip in stream:
+            if len(speech.shape) > 1:
+                speech = speech[:, 0] + speech[:, 1]
+            predicted = run_inference(speech_clip, model, processor, device=device)
+            arabic_to_en = translate_text(predicted)
+            predicted_text.append(re.sub(r'[^\w]', '', predicted))
+            translated_text.append(re.sub(r'[^\w]', '', arabic_to_en))
     else:
-        return run_inference(speech, model, processor, device=device)
-
-    return " ".join(text)
+        predicted = run_inference(speech, model, processor, device=device)
+        arabic_to_en = translate_text(predicted)
+        return predicted, arabic_to_en
+    return " ".join(translated_text), " ".join(predicted_text)
 
 
 def translate_text(text_data, model_name="facebook/nllb-200-distilled-600M", src_lang='arabic'):
     tokenizer = AutoTokenizer.from_pretrained(model_name, use_auth_token=True, src_lang=src_lang)
     model = AutoModelForSeq2SeqLM.from_pretrained(model_name, use_auth_token=True)
     inputs = tokenizer(text_data, return_tensors="pt")
-    translated_tokens = model.generate(**inputs,max_length=90,forced_bos_token_id=tokenizer.lang_code_to_id["eng_Latn"])
+    translated_tokens = model.generate(**inputs, max_length=1024,
+                                       forced_bos_token_id=tokenizer.lang_code_to_id["eng_Latn"])
     translated_text = tokenizer.batch_decode(translated_tokens, skip_special_tokens=True)[0]
     return translated_text
 
 
 # arabic model facebook/s2t-wav2vec2-large-en-ar
 def speech_to_text(audio_path, model_name='facebook/wav2vec2-base-960h'):
+    logger = create_logger()
+    logger.info("Processing {}".format(audio_path))
     translator = Translator()
     transcription = dict()
     # device = "cuda" if th.cuda.is_available() else "cpu"
     device = "cpu"
     wav2vec2_processor = Wav2Vec2Processor.from_pretrained(model_name)
     wav2vec2_model = Wav2Vec2ForCTC.from_pretrained(model_name).to(device)
-    transcription['text'] = get_transcription(audio_path, model=wav2vec2_model, processor=wav2vec2_processor,
-                                              device=device)
-    print(transcription['text'])
-    # arabic_to_en = translator.translate(transcription['text'],dest='en')
-
-    transcription['translate'] = arabic_to_en
+    transcription['translated'], transcription['text'] = get_transcription(audio_path, model=wav2vec2_model,
+                                                                           processor=wav2vec2_processor,
+                                                                           device=device)
 
     return transcription
 
 
-from tenacity import retry, wait_random
+def create_logger():
+    formatter = logging.Formatter('%(asctime)s:%(levelname)s:- %(message)s')
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+    console_handler.setFormatter(formatter)
 
-# @retry(wait=wait_random(min=5, max=10))
-# def translate_text(self, text, orginal_text='ar', convert_to='english'):
-#     prompt = f'Translate the following {orginal_text} text to {convert_to}:\n\n{orginal_text}: ' + text + '\n{convert_to}:'
-#     # Generate response using ChatGPT
-#     response = openai.Completion.create(
-#         engine='text-davinci-003',
-#         prompt=prompt,
-#         max_tokens=100,
-#         n=1,
-#         stop=None,
-#         temperature=0.7
-#     )
-#     # Extract the translated English text from the response
-#     translation = response.choices[0].text.strip()
-#     return translation
+    logger = logging.getLogger("APT_Realignment")
+    logger.setLevel(logging.INFO)
+
+    if not logger.hasHandlers():
+        logger.addHandler(console_handler)
+    logger.propagate = False
+    return logger
 
 
 if __name__ == '__main__':
-    url = "https://prypto-api.aswat.co/surveillance/recordings/44fe5e2b-4428-41a1-826b-5de427b7930e.mp3"
+    url = "https://prypto-api.aswat.co/surveillance/recordings/5a0e3bd9-f603-45b8-a086-8c38be251a73.mp3.mp3"
     text = speech_to_text(audio_path=url, model_name='facebook/s2t-wav2vec2-large-en-ar')
 
     print(text['text'])
-    print(text['translate'])
