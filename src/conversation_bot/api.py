d6d6d72889a1d985ed0a768aad2237c665f03eb4
samarth991 <samarthtandon91@gmail.com>
2023-07-09 22:34:50 +0530

    adding Audio processing API

--- src/conversation_bot/api.py
+++ src/conversation_bot/api.py
@@ -1,16 +1,19 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
-from Audio2Text.whisper_api import WHISPERModel
-from Audio2Text.huggingface_api import speech_to_text as huggingface_converter
+from Audio_api.whisper_api import WHISPERModel
+from Audio_api.huggingface_api import speech_to_text as huggingface_converter
+
 
 # Models
 class RequestBody(BaseModel):
     urls: List[str]
 
+
 class ResponseItem(BaseModel):
     id: int
     text: str
+
 
 class ResponseBody(BaseModel):
     data: List[ResponseItem]
@@ -26,20 +29,28 @@ api = FastAPI()
 async def root():
     return {"status": "API server is up and running"}
 
+
 @api.get("/whisper")
 async def whisper(req: RequestBody) -> ResponseBody:
     data = []
     for idx, url in enumerate(req.urls):
         text = whisper_model.speech_to_text(url)
-        text["id"] = idx+1
+        text["id"] = idx + 1
         data.append(text)
-    return { "data": data }
+    data_dict = {
+        "data" : data
+    }
+    return data_dict
+
 
 @api.get("/huggingface")
 async def huggingface(req: RequestBody) -> ResponseBody:
     data = []
     for idx, url in enumerate(req.urls):
         text = huggingface_converter(audio_path=url, model_name='facebook/s2t-wav2vec2-large-en-ar')
-        text["id"] = idx+1
+        text["id"] = idx + 1
         data.append(text)
-    return { "data": data }
+    data_dict = {
+        "data" : data
+    }
+    return data_dict
